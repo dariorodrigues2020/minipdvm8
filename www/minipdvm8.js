3589943a0012a4d6a090241b6f788b7ebc495962
dariorodrigues2020 <69046153+dariorodrigues2020@users.noreply.github.com>
2020-08-18 16:52:25 -0300

    Add files via upload

--- www/minipdvm8.js
+++ www/minipdvm8.js
@@ -1,5 +1,15 @@
 var exec = require( 'cordova/exec' );
 
+exports.open = function ( arg0, success, error ) 
+{
+    exec( success, error, 'minipdvm8', 'open', [ arg0 ] );
+};
+
+exports.close = function ( arg0, success, error ) 
+{
+    exec( success, error, 'minipdvm8', 'close', [ arg0 ] );
+};
+
 exports.printText = function ( arg0, success, error ) 
 {
     exec( success, error, 'minipdvm8', 'printText', [ arg0 ] );
